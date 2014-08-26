6a00daca0ecb5ea2aca7e91c7bab956d0a92dcae
l3x-venom <lex.sites@gmail.com>
2014-08-26 14:55:07 -0400

    add function adapter to httpserver.go

--- httpserver.go
+++ httpserver.go
@@ -157,6 +157,32 @@ size="50">
 </form>
 `
 
+func errorHandler(f func(http.ResponseWriter, *http.Request) error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("errorHandler...")
+		err := f(w, r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Printf("handling %q: %v", r.RequestURI, err)
+		}
+	}
+}
+
+func doThis() error { return nil }
+func doThat() error { return errors.New("ERROR - doThat") }
+
+func wrappedHandler(w http.ResponseWriter, r *http.Request) error {
+	log.Println("betterHandler...")
+	if err := doThis(); err != nil {
+		return fmt.Errorf("doing this: %v", err)
+	}
+
+	if err := doThat(); err != nil {
+		return fmt.Errorf("doing that: %v", err)
+	}
+	return nil
+}
+
 
 func main() {
 	cfg := jsoncfgo.Load("/Users/lex/dev/go/data/webserver/webserver-config.json")
@@ -181,6 +207,7 @@ func main() {
 
 	rdh := http.RedirectHandler("http://example.org", redirect_code)
 	mux.Handle("/redirect", rdh)
+	mux.Handle("/notFound", http.NotFoundHandler())
 
 	mux.Handle("/help", http.HandlerFunc( HelpHandler ))
 
@@ -189,6 +216,8 @@ func main() {
 
 	mux.Handle("/user/", http.HandlerFunc( UserHandler ))
 	mux.Handle("/ajax", http.HandlerFunc( AjaxHandler ))
+
+	mux.Handle("/adapter", errorHandler(wrappedHandler))
 
 	log.Printf("Running on port %d\n", port)
 
